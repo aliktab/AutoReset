b6198ad10d356bec633d27e169a804ae6eb428bd
Alik Tabunov <aliktab@gmail.com>
2016-01-22 23:45:20 +0300

    First simple version, final stage.

--- AutoReset.ino
+++ AutoReset.ino
@@ -22,29 +22,38 @@
 #include <PwCore.h>
 #include <Time.h>
 
-const uint8_t PIN_ONOFF  = 0x1;
+const uint8_t PIN_ONOFF  = 0x0;
 const uint8_t PIN_PING   = 0x2;
-const uint8_t PIN_RESET  = 0x0;
+const uint8_t PIN_RESET  = 0x1;
 
-const time_t PING_TIMEOUT = 9 * 1000;
+const time_t PING_TIMEOUT = 15;
+const time_t RESET_PAUSE  = 900;
+const time_t CYCLE_PAUSE  = 10;
 
-time_t curr_time = 0;
-time_t ping_time = 0;
+volatile time_t ping_time = 0;
 
 // Initialization
 
 void setup()
 {
+  // Initialize interrupts.
+  cli();                                // disables interrupts
+  GIMSK = 0b00100000;                   // turns on pin change interrupts
+  PCMSK = 0b00000100 | (1 << PIN_PING); // turn on interrupts on pins PIN_PING
+  sei();                                // enables interrupts
+
+  // Initialize time system.
+  setTime(0);
+  ping_time = now();
+
   // Initializeline pins mode, value, state and timer.
   pinMode(PIN_ONOFF, INPUT);
   pinMode(PIN_PING, INPUT);
 
   pinMode(PIN_RESET, OUTPUT);
   digitalWrite(PIN_RESET, HIGH);
-
-  // Initialize time system.
-  setTime(0);
-  ping_time = now();
+  delay(RESET_PAUSE);
+  digitalWrite(PIN_RESET, LOW);
 }
 
 
@@ -52,17 +61,30 @@ void setup()
 
 void loop()
 {
-  if (HIGH == digitalRead(PIN_ONOFF))
-  {
-    if (HIGH == digitalRead(PIN_PING))
-      ping_time = now();
-  }
-  else
+  if (LOW == digitalRead(PIN_ONOFF))
     ping_time = now();
 
-  digitalWrite(PIN_RESET, (now() - ping_time < PING_TIMEOUT) ? HIGH : LOW);
+  if (HIGH == digitalRead(PIN_PING))
+    ping_time = now();
+
+  if (now() - ping_time > PING_TIMEOUT)
+  {
+    digitalWrite(PIN_RESET, HIGH);
+    delay(RESET_PAUSE);
+    ping_time = now();
+  }
+
+  digitalWrite(PIN_RESET, LOW);
+  delay(CYCLE_PAUSE);
 }
 
+
+// Hold interrupt on PING
+
+ISR(PCINT0_vect)
+{
+  ping_time = now();
+}
 
 
 
